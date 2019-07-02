5415ee10f653f5d87577dda3fdb117baafd5cec9
Bryan Sng <sngby98@gmail.com>
2019-07-02 15:48:45 +0100

    pretty please

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import CounterButton from './CounterButton';
+import React, { Component } from "react";
+import CounterButton from "./CounterButton";
+import CounterButton2 from "./CounterButton2";
 
 class Header extends Component {
-	shouldComponentUpdate(nextProps, nextState) {
-		return false;
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  }
 
-	render() {
+  render() {
     return (
-			<div>
-				<h1 className="f1">RobotBuddies</h1>
-				<CounterButton color={'red'} />
-			</div>
+      <div>
+        <h1 className="f1">RobotBuddies</h1>
+        <CounterButton color={"red"} />
+        <CounterButton2 color={"red"} />
+      </div>
     );
-	}
+  }
 }
 
 export default Header;
